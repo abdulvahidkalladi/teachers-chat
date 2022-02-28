7d726737c270930b3550df4650afa08cf3516341
unknown <abdulvahidkalladi@gmail.com>
2022-02-28 19:36:55 +0400

    first commit

---
+++ client/src/components/TeamMessage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { MessageTeam, useMessageContext } from "stream-chat-react";
+
+const TeamMessage = () => {
+  const { handleOpenThread, message } = useMessageContext();
+
+  return <MessageTeam message={{ ...message, user: {} }} />;
+};
+
+export default TeamMessage;
